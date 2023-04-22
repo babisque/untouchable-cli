b911d5909589bdeb3d49a0b124ab87261e621702
Babisque <rodrigobabisque1@gmail.com>
2023-04-22 17:01:51 -0300

    new test for create schema

--- tests/CreateSchemaTest.php
+++ tests/CreateSchemaTest.php
@@ -19,4 +19,14 @@ final class CreateSchemaTest extends TestCase
         $result = $command->run();
         $this->assertEquals(1, $result);
     }
+
+    public function testUserCannotCreateASchemaWithSameName()
+    {
+        $pdo = new PDO('mysql:host=localhost', $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        $command = new CreateSchema($pdo);
+        $command->setSubcommandsOrArguments(['name' => 'test_db']);
+        $result = $command->run();
+
+        $this->assertEquals(1, $result);
+    }
 }
